8c8bc05b5ee172c461099e287417eb64da9794ed
Greg Schoeninger <greg.schoeninger@gmail.com>
2012-09-29 11:31:51 -0600

    working on maps

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,5 +1,10 @@
 class MapController < ApplicationController
 	def index
+		@month = Time.now.month
+		@day = Time.now.day
+		if params == {"controller"=>"map", "action"=>"index"} 
+			params.merge!({:month => @month, :day => @day})
+		end
 		@booked_pins = GuidePost.find_all_by_booking_status_id(BookingStatus.find_by_status("Booked"))
 		@pending_pins = GuidePost.find_all_by_booking_status_id(BookingStatus.find_by_status("Pending"))
 	end

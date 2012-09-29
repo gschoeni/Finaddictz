13ddec66ddaaa6cea4e0fad7d731d10be8c84dae
Greg Schoeninger <greg.schoeninger@gmail.com>
2012-09-29 12:04:55 -0600

    added searching params for type of posts on map, going to hook up routes for anglers to mark trips as booked next

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -4,10 +4,20 @@ class MapController < ApplicationController
 		@day = Time.now.day
 		@year = Time.now.year
 		if params == {"controller"=>"map", "action"=>"index"} 
-			params.merge!({:month => @month, :day => @day})
+			params.merge!({:month => @month, :day => @day, :type => "both"})
 		end
-		d = Time.parse("#{@year}-#{params[:month]}-#{params[:day]}")
-		@booked_pins = GuidePost.find_all_by_booking_status_id_and_date(BookingStatus.find_by_status("Booked"), d)
-		@pending_pins = GuidePost.find_all_by_booking_status_id_and_date(BookingStatus.find_by_status("Pending"), d)
+
+		search_date = Time.parse("#{@year}-#{params[:month]}-#{params[:day]}")
+		if params[:type] == "both"
+			@booked_pins = GuidePost.find_all_by_booking_status_id_and_date(BookingStatus.find_by_status("Booked"), search_date)
+			@pending_pins = GuidePost.find_all_by_booking_status_id_and_date(BookingStatus.find_by_status("Pending"), search_date)
+		elsif params[:type] == "angler"
+			@booked_pins = AnglerPost.find_all_by_booking_status_id_and_date(BookingStatus.find_by_status("Booked"), search_date)
+			@pending_pins = []
+		else
+			@booked_pins = GuidePost.find_all_by_booking_status_id_and_date(BookingStatus.find_by_status("Booked"), search_date)
+			@pending_pins = GuidePost.find_all_by_booking_status_id_and_date(BookingStatus.find_by_status("Pending"), search_date)
+		end
+		
 	end
 end
